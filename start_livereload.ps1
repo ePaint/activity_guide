ac9bd121c5d67457769a06b54b6221cd81892fb7
sebastian.rossetti <sebastian.rossetti@piano.io>
2023-12-15 23:56:14 +0100

    Added powershell scripts to start servers

---
+++ start_livereload.ps1
@@ -0,0 +1,8 @@
+# Navigate to your Django project directory
+cd $PSScriptRoot
+
+# Activate the virtual environment (if you are using one)
+.\venv\Scripts\activate.ps1  # Uncomment this line if you are using a virtual environment
+
+# Start the Django development server
+python manage.py livereload
