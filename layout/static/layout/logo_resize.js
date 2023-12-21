2f3e993e9b02ad6229890af37d6d7f6a3783c41c
sebastian.rossetti <sebastian.rossetti@piano.io>
2023-12-21 13:28:39 +0100

    Made site header dissappear when scrolling down on mobile

--- layout/static/layout/logo_resize.js
+++ layout/static/layout/logo_resize.js
@@ -1,18 +1,47 @@
 var logo_element = document.getElementById('site-logo');
-var max_size = 7;
-var min_size = 2;
-var size_unit = 'rem';
+var logo_max_size = 7;
+var logo_min_size = 2;
+var logo_size_unit = 'rem';
 
 function resizeLogo(page_y_offset) {
     var progress = Math.min(page_y_offset, 50) / 50;
+    var shrink_factor = progress * (logo_max_size - logo_min_size);
+    var logo_new_size = logo_max_size - shrink_factor;
+    logo_element.style.width = logo_new_size + logo_size_unit;
+    logo_element.style.height = logo_new_size + logo_size_unit;
+    logo_element.style.marginLeft = `calc(50% - ${(logo_new_size / 2)}${logo_size_unit})`;
+    logo_element.style.marginTop = 0.5 - 0.25 * progress + logo_size_unit;
+}
 
-    var shrink_factor = progress * (max_size - min_size);
-    var new_size = max_size - shrink_factor;
-    logo_element.style.width = new_size + size_unit;
-    logo_element.style.height = new_size + size_unit;
-    logo_element.style.marginLeft = `calc(50% - ${(new_size / 2)}${size_unit})`;
-    logo_element.style.marginTop = 0.5 - 0.25 * progress + size_unit;
+var header_element = document.getElementById('site-header');
+var header_max_size = 4.5;
+var header_min_size = 0;
+var header_size_unit = 'rem';
+var prev_page_y_offset = 0;
+
+function resizeHeader(page_y_offset) {
+    window.onscroll = () => {};
+    var distance = page_y_offset - prev_page_y_offset;
+    
+    if (distance > 0) {
+        header_element.style.top = '-4.5rem';
+        console.log('up')
+    } else {
+        header_element.style.top = '0';
+        console.log('down')
+    }
+
+    prev_page_y_offset = page_y_offset;
+    window.onscroll = () => handleScroll();
+}
+
+function handleScroll() {
+    if (window.innerWidth > 575) {
+        resizeLogo(window.pageYOffset);
+    } else {
+        resizeHeader(window.pageYOffset);
+    }
 }
 
 resizeLogo(window.pageYOffset);
-window.onscroll = () => resizeLogo(window.pageYOffset);
+window.onscroll = () => handleScroll();
