a22cd78cf8d8e7e97f2783cb5a863b5cbb77938e
sebastian.rossetti <sebastian.rossetti@piano.io>
2023-12-28 20:53:07 +0100

    Added sample member dashboard

--- members/urls.py
+++ members/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from members.views import member_profile, member_dashboard
 
 urlpatterns = [
-    path('sample/', member_profile, name='member-profile'),
+    # path('sample/', member_profile, name='member-profile'),
     path('sample/dashboard', member_dashboard, name='member-dashboard'),
 ]
