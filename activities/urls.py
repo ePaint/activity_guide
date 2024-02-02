89db8100946f7e94dce8e28b03a87d045fa8de7b
sebastian.rossetti <sebastian.rossetti@piano.io>
2024-02-02 23:27:41 +0100

    Temp

--- activities/urls.py
+++ activities/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from activities.views import activity_detail, activity_edit
+from activities.views import activity_detail, activity_edit, activity_field_edit
 
 urlpatterns = [
     path('<slug:slug>/', activity_detail, name='activity-detail'),
     path('<slug:slug>/edit', activity_edit, name='activity-edit'),
+    path('<slug:slug>/field/edit', activity_field_edit, name='activity-field-edit'),
 ]
--- activities/views.py
+++ activities/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.exceptions import ValidationError
 from activities.models import Activity
 
 
@@ -15,3 +17,23 @@ def activity_edit(request, slug):
         'activity': activity
     }
     return render(request, 'activities/activity_edit.html', context)
+
+def activity_field_edit(request, slug):
+    if (request.method != 'POST'):
+        return HttpResponse('')
+    field = request.POST.get('field')
+    value = request.POST.get('value')
+    prev_value = request.POST.get('prev_value')
+    activity = Activity.objects.get(slug=slug)
+    if (field not in activity.__dict__):
+        return HttpResponse(prev_value)
+    
+    activity.__dict__[field] = value
+
+    try:
+        activity.save()
+    except ValidationError:
+        return HttpResponse(prev_value)
+    
+    return HttpResponse(value)
+    
