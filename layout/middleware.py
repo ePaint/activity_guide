2b433af620f9c1603bdbfcb762ff6cf785082765
sebastian.rossetti <sebastian.rossetti@piano.io>
2024-02-23 21:05:02 +0100

    Changed HTMX cache management

--- layout/middleware.py
+++ layout/middleware.py
@@ -10,6 +10,7 @@ class HTMXMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         # if request.htmx:
         #     response['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
+        response['Cache-Control'] = 'max-age=' + str(60 * 60 * 24 * 365)
         return response
 
 
