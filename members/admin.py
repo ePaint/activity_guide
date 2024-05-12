c007944c39687199d7ed103d01699fd6bf8192c9
sebastian.rossetti <sebastian.rossetti@piano.io>
2024-05-12 02:54:04 +0200

    Added phone and email provider edit forms in provider dashboard

--- members/admin.py
+++ members/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Relationship, FamilyMember
+from .models import Member, FamilyMember, Relationship
 
 
+admin.site.register(Member)
 admin.site.register(Relationship)
 admin.site.register(FamilyMember)
